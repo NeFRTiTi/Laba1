942100f20818c9073576b2888ba74c51203868c4
Дмитрий Герасимчик <Gerasimchik_dima@mail.ru>
2020-06-03 15:59:15 +0300

    Комментарий к комиту

---
+++ ЛР1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ЛР1
+{
+
+    class Program
+    {
+        static void shellSort(int[] mass)
+        {
+            int step = mass.Length / 2;
+            while (step > 0)
+            {
+                int i, j;
+                for (i = step; i < mass.Length; i++)
+                {
+                    int value = mass[i];
+                    for (j = i - step; (j >= 0) && (mass[j] > value); j -= step)
+                        mass[j + step] = mass[j];
+                    mass[j + step] = value;
+                }
+                step /= 2;
+            }
+
+        }
+
+        static void IsInt(out int test)
+        {
+            while (!int.TryParse(Console.ReadLine(), out test))
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите массив:");
+            string s = Console.ReadLine();
+            string[] num = s.Split(' ');
+            int[] numbers = new int[num.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(num[i]);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Нажмите Enter, что бы начать сортировку");
+            Console.ReadLine();
+            shellSort(numbers);
+            Console.WriteLine("Готово! Теперь ваш массив выглядит следующим образом:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
